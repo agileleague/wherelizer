7b3d6b6752c9237ef95025001527a2846998c95b
Melinda Weathers <melinda@agileleague.com>
2013-05-22 22:31:51 -0400

    Additional test & todos

--- test/arelizer_test.rb
+++ test/arelizer_test.rb
@@ -7,12 +7,19 @@ describe Arelizer do
     assert_equal %q|WikiPage.where(:campaign_id => source_campaign.id).where(:name => target_names)|, arelizer.convert
   end
 
+  it 'handles a numbers and strings as conditions' do
+    arelizer = Arelizer.new( %q|WikiPage.all(:conditions => {:campaign_id => 5, :name => "Cool"})|)
+    assert_equal %q|WikiPage.where(:campaign_id => 5).where(:name => "Cool")|, arelizer.convert
+  end
+
   it 'maintains a query for "first"' do
     arelizer = Arelizer.new( %q|WikiPage.first(:conditions => {:campaign_id => source_campaign.id, :name => target_names})|)
     assert_equal %q|WikiPage.where(:campaign_id => source_campaign.id).where(:name => target_names).first|, arelizer.convert
   end
 
   #TODO: find(:all) and find(:first)
+  #TODO: check that strings are supported as well as symbols
+  #TODO: new hash syntax where we can
 
   it 'handles an order hash' do
     arelizer = Arelizer.new( %q|WikiPage.all(:order => 'name asc', :conditions => {:campaign_id => source_campaign.id, :name => target_names})|)
